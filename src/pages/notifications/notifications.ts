f3d5fefc2d62faed873e44a3130599950085c4fe
juanGro <ing.juangro@gmail.com>
2017-10-22 00:31:32 -0500

    Add new pages from ionic creator

---
+++ src/pages/notifications/notifications.ts
@@ -0,0 +1,32 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+// import { HTTP } from '@ionic-native/http';
+
+@Component({
+  selector: 'page-notifications',
+  templateUrl: 'notifications.html'
+})
+export class NotificationsPage {
+  // this tells the tabs component which Pages
+  // should be each tab's root Page
+  constructor(public navCtrl: NavController) {
+    /*
+    this.http.get('https://jsonplaceholder.typicode.com/posts/', {}, {})
+    .then(data => {
+  
+      console.log(data.status);
+      console.log(data.data); // data received by server
+      console.log(data.headers);
+  
+    })
+    .catch(error => {
+  
+      console.log(error.status);
+      console.log(error.error); // error message as string
+      console.log(error.headers);
+  
+    });
+    */
+  }
+  
+}
